94e16363e221891a4bd1e31616609291c24c018c
nasarali03 <nasarali1805@gmail.com>
2024-05-13 23:28:10 +0500

    ..

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import path from "path";
 const app = express();
 const PORT = 8000;
 
+const view_path = path.resolve("./views");
 app.set("view engine", "ejs");
-app.set("views", path.resolve("views"));
+app.set("views", view_path);
+
 console.log(path.resolve("views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
 app.listen(PORT, () => console.log(`Server is listening at ${PORT}`));
