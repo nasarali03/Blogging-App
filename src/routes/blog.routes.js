c7c40db43484dfe316f6208e63ce41651f2106c7
nasarali03 <nasarali1805@gmail.com>
2024-05-24 23:48:18 +0500

    Comments route

--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -1,12 +1,25 @@
 import Router from "express";
 import { upload } from "../middleware/multer.middleware.js";
-import { addBlog } from "../controllers/blog.controllers.js";
+import { addBlog, comments } from "../controllers/blog.controllers.js";
+import Blog from "../models/blog.model.js";
+import Comment from "../models/comments.model.js";
 
 const router = Router();
 
 router.get("/add-new", (req, res) => {
   res.render("addBlog", { user: req.user });
 });
+
+router.get("/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({ blogId: req.params.id }).populate(
+    "createdBy"
+  );
+
+  return res.render("blog", { user: req.user, blog: blog, comments: comments });
+});
+
+router.post("/comment/:blogId", comments);
 
 router.post("/", upload.single("coverImage"), addBlog);
 
