23651b6fac93c30df427c3283389304de58a03ce
nasarali03 <nasarali1805@gmail.com>
2024-05-24 23:47:05 +0500

    Comments controller

--- src/controllers/blog.controllers.js
+++ src/controllers/blog.controllers.js
@@ -1,18 +1,35 @@
 import Blog from "../models/blog.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.utils.js";
+import Comment from "../models/comments.model.js";
 
 const addBlog = async (req, res) => {
-  const { title, body } = req.body;
-  const response = await uploadOnCloudinary(req.file?.path);
-  const coverImageURL = response.url;
+  try {
+    const { title, body } = req.body;
+    const response = await uploadOnCloudinary(req.file?.path);
+    const coverImageURL = response.url;
 
-  const blog = await Blog.create({
-    title,
-    body,
-    coverImageURL,
-    createdBy: req.user._id,
-  });
-  return res.redirect(`/blog/${blog._id}`, { user: req.user, blog: blog });
+    const blog = await Blog.create({
+      title,
+      body,
+      coverImageURL,
+      createdBy: req.user._id,
+    });
+
+    // Use the correct signature for res.redirect
+    return res.redirect(`/blog/${blog._id}`);
+  } catch (error) {
+    console.error("Error adding blog:", error);
+    return res.status(500).send("Internal Server Error");
+  }
 };
 
-export { addBlog };
+const comments = async (req, res) => {
+  await Comment.create({
+    content: req.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+  return res.redirect(`/blog/${req.params.blogId}`);
+};
+
+export { addBlog, comments };
